041f6f397df988d30220c827440c5efc9dfed7c8
Alexander Ekstrand <aekstrand92@gmail.com>
2023-12-09 14:37:24 +0100

    Add day 9 problem and data

---
+++ AdventOfCode2023/Day09/Program.cs
@@ -0,0 +1,16 @@
+﻿var lines = File.ReadAllLines("Data.txt");
+// var lines = File.ReadAllLines("ExampleData.txt");
+
+First();
+Second();
+return;
+
+void First()
+{
+    Console.WriteLine();
+}
+
+void Second()
+{
+    Console.WriteLine();
+}
