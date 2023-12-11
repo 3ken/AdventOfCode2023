561e53b28c624f7205917fa2e0bf7a0bd1bb823a
Alexander Ekstrand <aekstrand92@gmail.com>
2023-12-11 01:08:29 +0100

    Day08:2 Work in progress

--- AdventOfCode2023/Day08/Program.cs
+++ AdventOfCode2023/Day08/Program.cs
@@ -2,7 +2,7 @@
 // var lines = File.ReadAllLines("ExampleData.txt");
 var data = GetData();
 
-First();
+// First();
 Second();
 return;
 
@@ -26,7 +26,59 @@ void First()
 
 void Second()
 {
-    Console.WriteLine();
+    var allStartPositions = GetAllStartPositions();
+    long steps = 0;
+    var instructionPosition = 0;
+    var stepsToMultiply = new List<long>();
+    var stepsToSum = new List<long>();
+
+    for (var i = 0; i < allStartPositions.Count; i++)
+    {
+        var startPosition = allStartPositions[i];
+        var allPassedPositions = new List<string>();
+        allPassedPositions.Add(startPosition);
+        var position = allStartPositions[i];
+        while (true)
+        {
+            position = data.Instructions[instructionPosition] == 'R'
+                ? data.PositionNodes[position].Item2 
+                : data.PositionNodes[position].Item1;
+            if (allPassedPositions.Contains(position))
+            {
+                if (instructionPosition == 0 || steps % instructionPosition == 0)
+                {
+                    // var indexOfPosition = allPassedPositions.IndexOf(position);
+                    stepsToMultiply.Add(steps);
+                    break;
+                }
+            }
+            else
+            {
+                allPassedPositions.Add(position);
+            }
+            steps++;
+            instructionPosition++;
+            if (instructionPosition > data.Instructions.Length - 1) instructionPosition = 0;
+        }
+        steps = 0;
+        instructionPosition = 0;
+    }
+
+    var lcm = stepsToMultiply[0];
+    for (var i = 1; i < stepsToMultiply.Count; i++)
+    {
+        lcm *= stepsToMultiply[i];
+    }
+    
+    Console.WriteLine(lcm);
+}
+
+List<string> GetAllStartPositions()
+{
+    return data.PositionNodes
+        .Where(p => p.Key.Last() == 'A')
+        .Select(p => p.Key)
+        .ToList();
 }
 
 Data GetData()
@@ -37,8 +89,8 @@ Data GetData()
     for (var i = 2; i < lines.Length; i++)
     {
         var key = lines[i].Split(" = ")[0];
-        var leftValue = lines[i].Split(" = ")[1].Split(", ")[0].Remove(0, 1);
-        var rightValue = lines[i].Split(" = ")[1].Split(", ")[1].Remove(3, 1);
+        var leftValue = lines[i].Split(" = ")[1].Split(", ")[0].Remove(0, 1).Trim();
+        var rightValue = lines[i].Split(" = ")[1].Split(", ")[1].Remove(3, 1).Trim();
         positionNodes.Add(key, new ValueTuple<string, string>(leftValue, rightValue));
     }
 
